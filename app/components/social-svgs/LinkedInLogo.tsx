7b744a592315a5bd97404eb23c28a586cee3e3ac
carolina <Carolinahs100@gmail.com>
2025-04-19 16:42:27 -0600

    typo in linkedin link.

--- app/components/social-svgs/LinkedInLogo.tsx
+++ app/components/social-svgs/LinkedInLogo.tsx
@@ -9,7 +9,7 @@ export default function LinkedInLogo({
   className?: string;
 }) {
   return (
-    <a href="www.linkedin.com/in/carolina-henriquez-silva">
+    <a href="wwww.linkedin.com/in/carolina-henriquez-silva">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width={width}
