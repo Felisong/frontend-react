ce1623411a519d1148178abd40acce0ecc11e919
carolina <Carolinahs100@gmail.com>
2025-05-05 21:30:11 -0600

    got a carousel from youtube/ cahtgpt

--- app/components/HomeSkillsSection.tsx/page.tsx
+++ app/components/HomeSkillsSection.tsx/page.tsx
@@ -1,19 +1,16 @@
 "use client";
 
+import useSkills from "@/app/hook/useSkills";
 import SkillsCarousel from "../skills/SkillsCarousel";
 
 export default function HomeSkillsSection() {
-  const slides = [
-    { name: "Test1 ", link: "https://picsum.photos/200/300", id: 1 },
-    { name: "Test2 ", link: "https://picsum.photos/200/300", id: 2 },
-    { name: "Test3 ", link: "https://picsum.photos/200/300", id: 3 },
-  ];
+  const { skills, loading } = useSkills();
 
   return (
     <main className="text-center">
       <h1>My Skills</h1>
-      <div className="max-w-lg">
-        <SkillsCarousel slides={slides} />
+      <div className="max-w-full ">
+        <SkillsCarousel skills={skills} />
       </div>
 
       <p>svg here as an absolute behind</p>
