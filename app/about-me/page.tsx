bbf33f6e3a78c6732359bbf09c8ae8f2524eebb5
carolina <Carolinahs100@gmail.com>
2025-08-02 00:02:46 -0600

    got about me svg up, and updated the sheep to be easier to animate later.

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,3 +1,4 @@
+import AboutMeSvg from "../components/general/AboutMeSvg";
 import ArrowDown from "../components/general/ArrowDown";
 import BackDrop from "../components/general/BackDrop";
 import SkillsCarousel from "../components/home-section/skills/SkillsCarousel";
@@ -8,7 +9,9 @@ export default function AboutMe() {
       <section>
         <h1 className="font-header text-center mx-8 my-4"> About Me</h1>
         <div className="flex flex-col items-center">
-          <div className="w-[300px] h-[300px] my-8 rounded-full bg-bright-yellow" />
+          <div className="w-[80%] h-auto my-8 rounded-full bg-bright-yellow">
+            <AboutMeSvg />
+          </div>
           <BackDrop description="I am a friendly woman with a multitude of skills to offer. Passionate, driven and friendly. I do my best, and am always striving to improve myself. I have found a love for coding, and hope my site shows that too!" />
         </div>
         <div className="flex justify-center">
---
+++ app/components/home-section/skills/SkillsAboutMe.tsx
@@ -0,0 +1,17 @@
+export default function SkillAboutMe() {
+  // = = = = = = =
+  //  = = = = = =
+  // = = = = = = =
+
+  // each row can be moveed independently of eachother, and otherwise will have a mild hover effect
+  // cut the array into 3 groups, make the middle group the center with -1 the others
+  // git a div that is 100% width, fit-content height. Position relative. Hidden overflow
+  // 3 divs inside , each will probably be their own component
+  // have each one be like 150% width, and span accross
+  // do a pop shift offscreen of the items and keep looping through them as the user moves through them
+  return (
+    <div>
+      <p>Meow Skills go here</p>
+    </div>
+  );
+}
